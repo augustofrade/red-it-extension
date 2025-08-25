4727c2fbe8f9b4cd4fe9dbebaec680cf74aa828c
augustofrade <aquaaticnaotrava@gmail.com>
2025-08-25 16:58:36 -0300

    Add new popup button to select extension removal mode - exchange of messages between background.js and popup.js

--- background.js
+++ background.js
@@ -1,5 +1,45 @@
-browser.browserAction.onClicked.addListener(() => {
-  browser.tabs.create({
-    url: browser.runtime.getURL("pages/home/index.html"),
+function handleMessages(messages) {
+  const messageList = Object.entries(messages);
+
+  browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log("Received message:", message);
+
+    for (const [key, handler] of messageList) {
+      if (
+        (typeof message === "string" && message === key) ||
+        message.type === key
+      ) {
+        return handler(message, sender, sendResponse);
+      }
+    }
   });
+}
+handleMessages({
+  "open-settings": () => {
+    browser.tabs.create({
+      url: browser.runtime.getURL("pages/home/index.html"),
+    });
+  },
+  "open-about": () => {
+    browser.tabs.create({
+      url: "https://github.com/augustofrade/red-it-extension",
+    });
+  },
+  "list-modes": () => {
+    return Promise.resolve({
+      purge: "Removes the content from the page",
+      cover: "Covers the content with a placeholder. You can hover to see it.",
+      hide: "Blanks the content",
+    });
+  },
+  "set-mode": (message) => {
+    return browser.storage.sync.set({ mode: message.newMode });
+  },
+  "get-mode": () => {
+    return new Promise((resolve) => {
+      browser.storage.sync.get("mode").then((data) => {
+        resolve(data.mode || "purge");
+      });
+    });
+  },
 });
---
+++ popup/popup.js
@@ -0,0 +1,57 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+Element.prototype.on = Element.prototype.addEventListener;
+
+var mode;
+
+function selectMode(newMode) {
+  browser.runtime
+    .sendMessage({
+      type: "set-mode",
+      newMode,
+    })
+    .then(() => {
+      mode = newMode;
+      $(".mode-list-wrapper").classList.remove("active");
+      $(".mode-list-label").textContent = `Mode: ${mode}`;
+    });
+}
+
+$("#settings-btn").on("click", () => {
+  browser.runtime.sendMessage("open-settings");
+});
+
+$("#about-btn").on("click", () => {
+  browser.runtime.sendMessage("open-about");
+});
+
+$("#mode-btn").on("click", function () {
+  this.classList.add("active");
+  const modeListWrapper = $(".mode-list-wrapper");
+  modeListWrapper.classList.toggle("active");
+
+  if (modeListWrapper.classList.contains("active")) {
+    $(".mode-list-label").textContent = "Select a mode";
+  } else {
+    $(".mode-list-label").textContent = `Mode: ${mode}`;
+  }
+});
+
+browser.runtime.sendMessage("get-mode").then((currentMode) => {
+  mode = currentMode;
+  $(".mode-list-label").textContent = `Mode: ${mode}`;
+});
+
+browser.runtime.sendMessage("list-modes").then((modes) => {
+  const modeList = $(".mode-list");
+
+  Object.entries(modes).forEach(([mode, description]) => {
+    const item = document.createElement("li");
+    item.className = "menu-item mode-list-item";
+    item.dataset.value = mode;
+    item.title = description;
+    item.textContent = mode.charAt(0).toUpperCase() + mode.slice(1);
+    item.on("click", () => selectMode(mode));
+    modeList.appendChild(item);
+  });
+});
