d1c5827c096e8c7495a0dff724923151181c3807
augustofrade <aquaaticnaotrava@gmail.com>
2025-08-25 14:42:37 -0300

    Add content handling/hiding

---
+++ content-scripts/reddit.js
@@ -0,0 +1,108 @@
+(async function (mode = "purge") {
+  class RegexHelper {
+    static fromArray(arr) {
+      const wordBoundaryChars = "[ ,.?!;:\"'()\\[\\]{}<>‘’]";
+      const str = arr.join("|");
+      return new RegExp(
+        wordBoundaryChars + "(" + str + ")" + wordBoundaryChars,
+        "gi"
+      );
+    }
+  }
+
+  class ContentHandler {
+    static blocklistRegex = "";
+    static mode = "cover";
+    static metrics = {
+      blockedPosts: 0,
+    };
+
+    /**
+     * Reads user-defined settings from storage
+     */
+    static async init() {
+      let blockListString = (await browser.storage.sync.get("blocklist"))
+        .blocklist;
+      if (blockListString?.every === undefined) blockListString = [];
+
+      const regex = RegexHelper.fromArray(blockListString).source;
+      console.log(regex);
+
+      this.blocklistRegex = new RegExp(regex, "gi");
+      console.log("Blocklist regex:", this.blocklistRegex);
+      console.info("[RED-IT] Loaded blocklist");
+    }
+
+    static async handleMetrics() {
+      console.log(`[RED-IT] Blocked ${this.metrics.blockedPosts} posts.`);
+      if (this.metrics.blockedPosts > 0) {
+        document.title = `(${this.metrics.blockedPosts}) ${document.title}`;
+      }
+    }
+
+    /**
+     * Handles a single post on the page
+     * @param {HTMLElement} post
+     * @param {HTMLElement} title
+     */
+    static handlePost(post, title) {
+      const postTitle = title.innerText;
+      const titleString = " " + postTitle + " ";
+      if (titleString.match(this.blocklistRegex) === null) return;
+
+      console.log(`[RED-IT] Detected post: "${postTitle}"`);
+      this.metrics.blockedPosts++;
+
+      this.resetPost(post);
+      if (this.mode === "purge") {
+        post.style.display = "none";
+      } else if (this.mode === "cover") {
+        post.classList.add("unwanted-post");
+      } else if (this.mode === "hide") {
+        post.style.visibility = "hidden";
+      }
+    }
+
+    /**
+     * Resets the styles of a post element
+     * @param {HTMLElement} post
+     */
+    static resetPost(post) {
+      post.style.display = "block";
+      post.classList.remove("unwanted-post");
+      post.style.visibility = "initial";
+    }
+  }
+
+  class OldReddit {
+    static hostname = "old.reddit.com";
+
+    static handle() {
+      console.log("[RED-IT] Handling posts for " + this.hostname);
+      this._handlePosts();
+    }
+
+    static _handlePosts() {
+      for (let post of document.querySelectorAll("#siteTable .thing")) {
+        const titleEl = post.querySelector(".title:last-of-type");
+        ContentHandler.handlePost(post, titleEl);
+      }
+    }
+  }
+
+  // let detectedPosts = 0;
+  // console.log("Running Red-It in mode:", mode);
+
+  // console.log(`${detectedPosts} hidden posts.`);
+  // document.title = `(${detectedPosts}) ${document.title}`;
+
+  let site;
+  await ContentHandler.init();
+  if (location.hostname === OldReddit.hostname) {
+    site = OldReddit;
+  }
+  site.handle();
+  ContentHandler.handleMetrics();
+
+  return detectedPosts;
+})("cover");
