69e40fc5bba7872b8dc0f1dfae4747c9eac82326
augustofrade <aquaaticnaotrava@gmail.com>
2025-08-26 22:09:43 -0300

    Add subreddit page access blocking

---
+++ src/background-scripts/subreddit-blocker.js
@@ -0,0 +1,51 @@
+var blockedSubreddits = [];
+
+function normalizeSubredditList(list) {
+  return list?.map((item) => item.toLowerCase()) ?? [];
+}
+
+browser.storage.sync.get("subredditBlocklist").then((data) => {
+  blockedSubreddits = normalizeSubredditList(data.subredditBlocklist);
+});
+
+browser.storage.onChanged.addListener((changes, area) => {
+  if (area === "sync" && changes.subredditBlocklist) {
+    blockedSubreddits = normalizeSubredditList(
+      changes.subredditBlocklist.newValue
+    );
+  }
+});
+
+function blockSubredditPage(details) {
+  const url = new URL(details.url);
+  if (url.pathname === "/") {
+    return {};
+  }
+  const subreddit = url.pathname.split("/")[2];
+  const normalizedSubreddit = subreddit?.toLowerCase();
+
+  if (subreddit === undefined) {
+    return {};
+  }
+
+  if (blockedSubreddits.includes(normalizedSubreddit) === false) {
+    return {};
+  }
+
+  console.log("Blocking subreddit page:", subreddit);
+  const redirectUrl = browser.runtime.getURL(
+    `pages/blocked-subreddit/index.html?subreddit=${subreddit}`
+  );
+
+  console.log("Redirecting to:", redirectUrl);
+
+  return { redirectUrl };
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+  blockSubredditPage,
+  {
+    urls: ["*://*.reddit.com/r/*"],
+  },
+  ["blocking"]
+);
