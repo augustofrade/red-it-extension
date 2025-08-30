04a13cf6a027d3b7f59a28a62994ea6823c49d52
augustofrade <aquaaticnaotrava@gmail.com>
2025-08-30 16:56:33 -0300

    Add new reddit posts from top carousel blocing

--- src/content-scripts/reddit.js
+++ src/content-scripts/reddit.js
@@ -189,14 +189,16 @@
 
     static async handle() {
       console.log("[RED-IT] Handling posts for " + this.hostname);
-      this._handlePosts();
+      this._handleFeedPosts();
+      this._handleTopCarouselPosts();
     }
 
     static _listenFeedForPosts(element) {
       const observer = new MutationObserver((mutations) => {
         for (let mutation of mutations) {
           for (let node of mutation.addedNodes) {
-            if (node.nodeType !== Node.ELEMENT_NODE || node.tagName !== "ARTICLE") continue;
+            if (node.nodeType !== Node.ELEMENT_NODE) continue;
+            if (node.tagName !== "ARTICLE") continue;
             this._handleSinglePost(node);
           }
         }
@@ -204,17 +206,26 @@
       observer.observe(element, { childList: true, subtree: true });
     }
 
-    static async _handlePosts() {
+    static _handleFeedPosts() {
       const feed = document.querySelector("shreddit-feed");
       for (let post of feed.children) {
         if (post.tagName !== "ARTICLE") continue;
         this._handleSinglePost(post);
       }
-      // New reddit initially renders with only 3 articles
+      // New reddit is initially rendered with only 3 articles
       this._listenFeedForPosts(feed);
     }
 
-    static async _handleSinglePost(post) {
+    static _handleTopCarouselPosts() {
+      const carouselPosts = document.querySelectorAll("shreddit-gallery-carousel * > li");
+      for (let post of carouselPosts) {
+        const title = post.querySelector("h2").textContent.trim();
+        const subreddit = post.querySelector("span.font-bold").textContent.trim();
+        ContentHandler.handlePost(post, title, false, subreddit);
+      }
+    }
+
+    static _handleSinglePost(post) {
       const title = post.querySelector("faceplate-screen-reader-content").textContent.trim();
       const subreddit = post.querySelector("faceplate-hovercard a > span").textContent;
       ContentHandler.handlePost(post, title, false, subreddit);
